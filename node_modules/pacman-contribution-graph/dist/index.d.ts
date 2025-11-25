8c3db3783199c83473187779df84f4de4f13b8ef
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2025-11-25 16:22:06 +0000

    Revert "Readme"
    This reverts commit f2ef4ca7d60bb19a7144cb6e7917efc6f7164b14.

--- node_modules/pacman-contribution-graph/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Config, StoreType } from './types';
-export declare class PacmanRenderer {
-    store: StoreType;
-    conf: Config;
-    constructor(conf: Config);
-    start(): Promise<StoreType>;
-    stop(): void;
-}
